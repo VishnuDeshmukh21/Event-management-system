38a07cb4371c1f90e475a27a93803b6fb792afc1
vishnu <vishnudeshmukh1222@gmail.com>
2024-03-28 22:56:32 +0530

    ReadMe

--- events/urls.py
+++ events/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import event_find,event_add,hello
 urlpatterns = [
-    path('list',event_find),
+    path('find',event_find),
     path('add',event_add),
     path('',hello)
 ]
